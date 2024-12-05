25d6da34520dd2ddff5d7de2d997ce0b789a898e
jadenmecham <jadenzmecham@gmail.com>
2024-12-05 10:53:43 -0800

    test

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 import numpy as np
 
-print('test')
-A = np.array([1,1,1])
-print(A)
+C = np.array([[1,0,0],[1,1,0],[1,1,1],[1,0,1]])
+C1 = np.array([C[0],C[1],C[2]])
+C2 = np.array([C[1],C[2],C[3]])
+C3 = np.array([C[0],C[1],C[3]])
+C4 = np.array([C[0],C[2],C[3]])
+print(C1)
+print(C2)
+print(C3)
+print(C4)
