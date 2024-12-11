2f8e0c7d6b09dff9dbe1d8bd04978d4a995666b1
Jaden Mecham <112149558+jadenmecham@users.noreply.github.com>
2024-12-11 10:11:35 -0800

    add cart-pend sys

--- test.py
+++ test.py
@@ -2,14 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal as signal
 import control
+from scipy.integrate import solve_ivp
 
-m = 1
-M = 5
-L = 2
-g = -10
-d = 1
-s = -1
+m = 1 # mass of the pend
+M = 5 # mass of the cart
+L = 2 # length of the pendulum 
+g = -9.81 # gravity 
+d = 1 # damping
+s = -1 # starting position
 
+# state space matrices
 A = np.array([[0, 1, 0, 0], 
               [0, -d/M, -m*g/M, 0],
               [0, 0, 0, 1],
@@ -25,11 +27,44 @@ C = np.array([[1,0,0,0],
               [0,0,1,0],
               [0,0,0,1],
               [2,0,0,0],
-              [0,2,0,0]
-              [0,0,2,0]
-              [0,0,0,2]
+              [0,2,0,0],
+              [0,0,2,0],
+              [0,0,0,2],
               [1,0,1,0],
               [0,1,0,1]])
 
-D = np.array[0]
+D = np.zeros((4,1))
 
+# define the system 
+def state_space(t, x, u):
+    xdot = A @ x + B.flatten() * u
+    return xdot
+
+# Simulation parameters
+t_span = (0, 10)  # Time range for simulation
+x0 =  [0,0,0,0]#[0, 0, np.pi / 4, 0]  # Initial state: [x, dx, theta, dtheta]
+u = 1  # input force
+
+# Solve the system using ODE solver
+sol = solve_ivp(state_space, t_span, x0, args=(u,), t_eval=np.linspace(0, 10, 100))
+
+# Plot results
+plt.figure(figsize=(12, 8))
+plt.subplot(2, 1, 1)
+plt.plot(sol.t, sol.y[0], label="Cart Position (x)")
+plt.plot(sol.t, sol.y[2], label="Pendulum Angle (theta)")
+plt.xlabel("Time (s)")
+plt.ylabel("Position/Angle")
+plt.legend()
+plt.grid()
+
+plt.subplot(2, 1, 2)
+plt.plot(sol.t, sol.y[1], label="Cart Velocity (dx)")
+plt.plot(sol.t, sol.y[3], label="Pendulum Angular Velocity (dtheta)")
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity")
+plt.legend()
+plt.grid()
+
+plt.tight_layout()
+plt.show()
